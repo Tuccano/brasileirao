8f89e79c3c20fb5f4e6c85863e6ce7008741f2ac
tuccano <53663956+tuccano@users.noreply.github.com>
2019-08-04 14:18:07 -0300

    Update ex73.py

--- ex73.py
+++ ex73.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 times = ('Santos', 'Palmeiras', 'Flamengo',
          'Internacional', 'Atlético MG', 'São Paulo',
          'Corinthians', 'Atlético PR', 'Botafogo',
@@ -5,28 +7,73 @@ times = ('Santos', 'Palmeiras', 'Flamengo',
          'Ceará', 'Vasco', 'Fluminense', 'Cruzeiro',
          'Chapecoense', 'CSA', 'Avaí')
 
-op = 0
+janela = Tk()
 
-while op != 5:
-    print("[ 1 ] 5 Primeiros colocados\n"
-        "[ 2 ] 4 Últimos colocados\n"
-        "[ 3 ] Times em ordem alfbética\n"
-        "[ 4 ] Colocação de um time\n"
-        "[ 5 ] Sair")
-    print('=' * 30)
-    op = int(input("Digite sua opção: "))
+def bt1_click():
+    toptimes = Tk()
+    toptimes.geometry("200x105")
+    toptimes.title("5 Primeiros")
+    lb1 = Label(toptimes, text=f"1. {times[1]}")
+    lb2 = Label(toptimes, text=f"2. {times[2]}")
+    lb3 = Label(toptimes, text=f"3. {times[3]}")
+    lb4 = Label(toptimes, text=f"4. {times[4]}")
+    lb5 = Label(toptimes, text=f"5. {times[5]}")
+    lb1.pack()
+    lb2.pack()
+    lb3.pack()
+    lb4.pack()
+    lb5.pack()
 
-    if op == 1:
-        print(times[:5])
-    elif op == 2:
-        print(times[-4:])
-    elif op == 3:
-        print(sorted(times))
-    elif op == 4:
-        time = str(input('Digite o nome do time: '))
-        if time in times:
-            print(f'{time} está na {times.index(time) + 1}ª posição')
+def bt2_click():
+    worstimes = Tk()
+    worstimes.geometry("200x105")
+    worstimes.title("5 Últimos")
+    lb1 = Label(worstimes, text=f"20. {times[-1]}")
+    lb2 = Label(worstimes, text=f"19. {times[-2]}")
+    lb3 = Label(worstimes, text=f"18. {times[-3]}")
+    lb4 = Label(worstimes, text=f"17. {times[-4]}")
+    lb5 = Label(worstimes, text=f"16. {times[-5]}")
+    lb1.pack()
+    lb2.pack()
+    lb3.pack()
+    lb4.pack()
+    lb5.pack()
+
+def bt3_click():
+    select = Tk()
+    et = Entry(select)
+    def time():
+        if et.get() in times:
+            lr["text"] = f"{et.get()} está na {times.index(et.get()) + 1}ª posição"
         else:
-            print('Time inválido tente novamente')
-    elif op > 5 or op < 1:
-        print('Opção inválida tente novamente')
+            lr["text"] = "Time inválido tente novamente"
+
+    select.geometry("200x200")
+    select.title("Colocação do time")
+    txt = Label(select, text="Nome do time")
+    bt = Button(select, width=10, text="OK", command=time)
+    lr = Label(select, text="")
+    txt.pack()
+    et.pack()
+    bt.pack()
+    lr.pack()
+    
+#Propriedades da janela
+wjanela = 600
+hjanela = 200
+janela.geometry(f"{wjanela}x{hjanela}")
+janela.title("vem de fut")
+
+#Cabeçalho
+lb = Label(janela, text="Brasileirão 2019", bg="white")
+lb.pack(side=TOP, fill=X)
+
+#Layout
+bt1 = Button(janela, text="5 Primeiros colocados", command=bt1_click)
+bt1.pack(side=LEFT, anchor=N)
+bt2 = Button(janela, text="5 Últimos colocados", command=bt2_click)
+bt2.pack(side=LEFT, anchor=N)
+bt3 = Button(janela, text="Colocação de um time", command=bt3_click)
+bt3.pack(side=LEFT, anchor=N)
+
+janela.mainloop()
